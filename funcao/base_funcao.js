9f01c77e12b933e41444454413339420f833cba7
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-06-25 16:38:10 -0300

    função como parametro

--- funcao/base_funcao.js
+++ funcao/base_funcao.js
@@ -7,3 +7,10 @@ const fun2 = function () { }
 const obj = {}
 obj.falar = function () { return 'Opa'}
 console.log(obj.falar())
+
+// Função como parametro
+function run(fun){
+    fun()
+}
+
+run(function () { console.log('Executando ... ')})

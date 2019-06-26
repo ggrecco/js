8e83871ea98b0acc944ddac63acd745d89e72082
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-06-26 16:18:12 -0300

    eventos de texto e stickers

--- curso-bot/exercicios/eventos.js
+++ curso-bot/exercicios/eventos.js
@@ -11,6 +11,12 @@ bot.on('text', ctx => {
     ctx.reply(`Texto '${ctx.update.message.text}' recebido!`)
 })
 
+bot.on('sticker', ctx => {
+    const sticker = ctx.update.message.sticker
+    console.log(sticker)
+    ctx.reply(`Estou vendo que você enviou 
+    o ${sticker.emoji} do conjunto ${sticker.set_name}`)
+})
 
 
 bot.startPolling()

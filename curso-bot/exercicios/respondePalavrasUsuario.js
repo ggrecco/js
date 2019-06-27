ebe8c5d8a1fac27351a4c2691bf0bfaf8631e7f4
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-06-27 08:41:03 -0300

    bot com regex para dias da semana e responde texto

--- curso-bot/exercicios/respondePalavrasUsuario.js
+++ curso-bot/exercicios/respondePalavrasUsuario.js
@@ -8,5 +8,9 @@ const moment = require('moment')
 
 bot.hears(/burger/i, ctx => ctx.reply('Sim, porfavor'))
 bot.hears([/pqp/i, /fdp/i, /tnc/i], ctx => ctx.reply(`Coisa Feia ${ctx.update.message.from.first_name}!`))
-
+bot.hears(/(\d{2}\/\d{2})\/\d{4}/, ctx =>{
+    moment.locale('pt-BR')
+    const data = moment(ctx.match[1], 'DD/MM/YYYY')
+    ctx.reply(`${ctx.match[1]} é ${data.format('dddd')}`)
+})
 bot.startPolling()

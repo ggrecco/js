46f1a0d27351bd0c5b6c0a82a8634b0a3e92f03e
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-06-27 11:49:35 -0300

    criado menuzinho de ajuda ou de opções

---
+++ curso-bot/exercicios/menuDeComandos.js
@@ -0,0 +1,24 @@
+const env = require('../.env')
+const Telegraf = require('telegraf')
+const bot = new Telegraf(env.token)
+
+bot.start(ctx => {
+    const nome = ctx.update.message.from.first_name
+    ctx.reply(`Seja bem vindo ${nome}!\nSe precisar de /ajuda`)
+})
+
+
+bot.command('ajuda', ctx => ctx.reply('/ajuda: vou msotrar as opções'
+    + '\n/ajuda2: para testar via hears'
+    + '\n/op2: Opção genérica'
+    + '\n/op3: Outra opção genérica qualquer'))
+
+
+bot.hears('/ajuda2', ctx => ctx.reply('Eu também consigo capturar comandos'
+    + ',mas utilize a /ajuda mesmo'))
+
+// bot.hears(/\/op\d+/i, ctx => ctx.reply('Resposta padrão para comandos genéricos!'))
+//está expressão /\/op\d+/i serve para obrigar a giditação de um numero qualquer
+bot.hears(/\/op(2|3)/i, ctx => ctx.reply('Resposta padrão para comandos genéricos!'))
+
+bot.startPolling()

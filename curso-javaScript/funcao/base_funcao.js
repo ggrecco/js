8465aa1faf17cc917c4d197bc7a43f5696e3c91e
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-06-27 23:35:49 -0300

    função em atributo de objeto

--- curso-javaScript/funcao/base_funcao.js
+++ curso-javaScript/funcao/base_funcao.js
@@ -4,9 +4,12 @@ function fun1() { }
 //função anonima ou seja função que é instanciada em uma constante
 const fun2 = function () { }
 
+//Armazenada em um atributo de objeto
 const obj = {}
 obj.falar = function () { return 'Opa'}
+obj.somar = function (n1, n2) {return n1 + n2}
 console.log(obj.falar())
+console.log(obj.somar(9, 1))
 
 // Função como parametro
 function run(fun){
@@ -17,3 +20,4 @@ run(function () { console.log('Executando ... ')})
 
 //Armazenada em um array
 const array = [function (a, b) { return a + b}, fun1, fun2]
+console.log(array[0](2, 3))

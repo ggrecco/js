8457ec606f94db14d00e8dfecfb8001ca607dfcb
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-06-28 19:31:10 -0300

    varios tipos de parametros para uma função

--- curso-javaScript/funcao/parametros.js
+++ curso-javaScript/funcao/parametros.js
@@ -1,3 +1,20 @@
+//parametros em um array
+function soma() {
+    let soma = 0
+    for (i in arguments) {
+        soma += arguments[i]
+    }
+    return soma
+}
+
+console.log(soma())
+console.log(soma(1))
+console.log(soma(1.1, 2.2, 3.3))
+
+console.log(soma(1.1, 2.2, "Teste"))
+console.log(soma('a', 'b', 'c'))
+
+
 
 // estrategia 1 para gerar valor padrão
 function soma1(a, b, c) {
@@ -13,7 +30,7 @@ console.log(soma1(), soma1(3), soma1(1, 2, 3), soma1(0, 0, 0))
 function soma2(a, b, c) {
     a = a !== undefined ? a : 1
     b = 1 in arguments ? b : 1
-    c = isNaN(c) ? 1 : c //melhor solução
+    c = isNaN(c) ? 1 : c //melhor soluçã
     return a + b + c
 }
 
@@ -25,3 +42,20 @@ function soma3(a = 1, b = 1, c = 1) {
 }
 
 console.log(soma3(), soma3(3), soma3(1, 2, 3), soma3(0, 0, 0))
+
+
+//parametros são opconais
+function area(largura, altura) {
+    const area = largura * altura
+    if (area > 20) {
+        console.log(`Valor acima do permitido: ${area}m2.`)
+    } else {
+        return area
+    }
+}
+
+console.log(area(2, 2))
+console.log(area(2))
+console.log(area())
+console.log(area(2, 3, 17, 22, 44))
+console.log(area(5, 5))

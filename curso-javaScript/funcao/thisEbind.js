6d2b6d28efb2b3b9258fb0f7fd745ee289e03dbf
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-06-29 20:44:35 -0300

    usando o bind para corrigir o this

---
+++ curso-javaScript/funcao/thisEbind.js
@@ -0,0 +1,26 @@
+// problema de usar o this
+const pessoa = {
+    saudacao: 'Bom dia!',
+    falar() {
+        console.log(this.saudacao)
+    }
+}
+
+pessoa.falar()
+const falar = pessoa.falar
+falar() //gera conflito entre os paradigams de OO e funcional
+
+//resolvendo o erro undefined
+const falarDePessoa = pessoa.falar.bind(pessoa)
+falarDePessoa()
+
+function Pessoa() {
+    this.idade = 0
+
+    setInterval(function() {
+        this.idade++
+        console.log(this.idade)
+    }.bind(this), 1000)
+}
+
+new Pessoa

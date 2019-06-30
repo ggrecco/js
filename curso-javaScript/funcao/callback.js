fd2f1d937920759b4a9a8d5ca141999946d923d7
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-06-30 14:57:50 -0300

    callback

--- curso-javaScript/funcao/callback.js
+++ curso-javaScript/funcao/callback.js
@@ -10,3 +10,36 @@ const imprimirArrow = (nome, indice) =>  console.log(`${indice + 1}. ${nome}`)
 fabricantes.forEach(imprimir)
 fabricantes.forEach(imprimirArrow)
 fabricantes.forEach(elementoArray => console.log(elementoArray))
+
+const notas = [7.7, 6.5, 5.4, 8.9, 3.6, 7.1, 9.0]
+//sem callback
+let notasBaixas = []
+for (let i in notas) {
+    if (notas[i] < 7){
+        notasBaixas.push(notas[i])
+    }
+}
+
+console.log(notasBaixas)
+
+notasBaixas = notas.filter(function(nota){
+    return nota < 7
+})
+console.log(notasBaixas)
+
+notasBaixas = notas.filter(nota => nota < 7)
+console.log(notasBaixas)
+
+const notaMenor = valor => valor < 7
+
+notasBaixas = notas.filter(notaMenor)
+console.log('notaMenor: ' + notasBaixas)
+
+
+//exemplo de callback no browser
+document.getElementsByTagName('body')[0].onclick = function(e){
+    console.log('O evento ocorreu!')
+}
+
+
+document.getElementsByTagName('body')[0].onclick = evento => console.log('Evento')

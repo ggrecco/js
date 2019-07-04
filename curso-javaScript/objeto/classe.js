a6f673269efb181edeebd881ff999ff79531d22c
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-07-04 17:03:12 -0300

    começando as classes

---
+++ curso-javaScript/objeto/classe.js
@@ -0,0 +1,30 @@
+class Lancamentos {
+    constructor(nome = 'Genérico', valor = 0) {
+        this.nome = nome
+        this.valor = valor
+    }
+}
+
+class CicloFinanceiro {
+    constructor(mes, ano){
+        this.mes = mes
+        this.ano = ano
+        this.lancamentos = []
+    }
+
+    addLancamentos(...lancamentos){
+        lancamentos.forEach(l => this.lancamentos.push(l))
+    }
+
+    sumario() {
+        let valorConsolidado = 0
+        this.lancamentos.forEach(l => {
+            valorConsolidado += l.valor
+        })
+        return valorConsolidado
+    }
+}
+
+const salario = new Lancamentos('Salario', 4500)
+const contaDeLuz = new Lancamentos('Luz', -220)
+const contas = new CicloFinanceiro(6, 2018)

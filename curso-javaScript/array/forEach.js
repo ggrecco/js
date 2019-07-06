6aa273cd675c82b871e58b5cd9bda68b32a31516
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-07-06 14:02:24 -0300

    minha tentativa

--- curso-javaScript/array/forEach.js
+++ curso-javaScript/array/forEach.js
@@ -31,4 +31,36 @@ aprovados.forEach2(function(nome, indice, array){
     console.log(array)
 })
 
+// map
+// transforma uma array em outro
+console.log('===MAP===')
+const num = [1, 2, 3, 4, 5, 6]
 
+let resultado = num.map(e => {
+    return e * 2
+})
+console.log('-----Dobro-----')
+console.log(resultado)
+
+const soma10 = e => e + 10
+const triplo = e => e * 3
+const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}` // immprime no formato R$ XX,XX
+
+resultado = num.map(soma10).map(triplo).map(paraDinheiro)
+console.log('-----varios maps-----')
+console.log(resultado)
+
+// desafio
+
+const carrinho = [
+    '{"nome": "Borracha", "preco": 3.45}',
+    '{"nome": "Caderno", "preco": 13.90}',
+    '{"nome": "Kit de Lapis", "preco": 41.22}',
+    '{"nome": "Caneta", "preco": 7.50}']
+
+// Retorna o aray apenas com os preços
+
+let precos = JSON.parse(carrinho)
+precos = precos.map(precos.preco)
+console.log('-----desafio----')
+console.log(typeof precos)

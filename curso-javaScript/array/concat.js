eac48dbc5cfd1830b319f1be4a5a88894a15da47
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-07-07 14:04:08 -0300

    concatenando arrays com arrays e elementos

---
+++ curso-javaScript/array/concat.js
@@ -0,0 +1,8 @@
+// concatena arrays
+const filhas = ['Ualeskah', 'Cibalena']
+const filhos = ['Uoxiton', 'Uesclei']
+const todos = filhos.concat(filhas, 'Fulano')
+
+console.log(todos)
+
+console.log(['a'].concat([1, 2], [3, 4], 5, [[6, 7]]))

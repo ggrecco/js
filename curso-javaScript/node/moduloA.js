149e8e1070179e12e85428e5a145a8fc69e6168e
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-07-09 14:28:37 -0300

    exportar e importar

---
+++ curso-javaScript/node/moduloA.js
@@ -0,0 +1,4 @@
+// exportando node
+this.ola = 'Hello World'
+exports.ola2 = 'Hello World 2'
+module.exports.ola3 = 'Hello World 3'
---
+++ curso-javaScript/node/moduloCliente.js
@@ -0,0 +1,11 @@
+const moduloA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.ola2)
+console.log(moduloA.ola3)
+console.log(moduloA)
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)

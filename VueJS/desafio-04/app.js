5a63f2e60d18141b21eeeaccbec269d1b8897069
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-08-09 12:48:32 -0300

    minha solução ex1

--- VueJS/desafio-04/app.js
+++ VueJS/desafio-04/app.js
@@ -1,11 +1,12 @@
 new Vue({
 	el: '#desafio',
 	data: {
-
+		meuCSS: ''
 	},
 	methods: {
 		iniciarEfeito() {
-
+			setInterval(() => { this.meuCSS = 'encolher' }, 3000),
+				setInterval(() => { this.meuCSS = 'destaque' }, 5000)
 		},
 		iniciarProgresso() {
 

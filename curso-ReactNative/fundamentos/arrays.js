2d43cbdfb964b78eab267fff9dbf9539ed333dd2
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-06-05 16:46:04 -0300

    manipulando json dentro de array

--- curso-ReactNative/fundamentos/arrays.js
+++ curso-ReactNative/fundamentos/arrays.js
@@ -9,3 +9,17 @@ const aprovados = ['Gustavo', 'Grecco', 'Karim', 'Pinedo']
 aprovados.forEach(function(nome, indice) {
     console.log(`${indice + 1}. ${nome}`)
 })
+
+// concertendo um array para JSON e buscando um valor específico
+const carrinho = [
+    '{"nome": "Borracha", "preco": "3.45" }',
+    '{"nome": "Caderno", "preco": "13.90" }',
+    '{"nome": "Kit de Lapis", "preco": "41.22" }',
+    '{"nome": "Caneta", "preco": "7.50" }',
+]
+
+const newObjeto = json => JSON.parse(json)
+const sohPreco = produto => produto.preco
+
+const resultado = carrinho.map(newObjeto).map(sohPreco)
+console.log(resultado)

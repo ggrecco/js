f46e7f327e24e69aebc43c29ab3437031754f673
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-06-13 15:48:37 -0300

    craindo menu laterar arrastar e botões

---
+++ curso-reactNative/exercicios/src/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {createDrawerNavigator} from 'react-navigation'
+
+import Simples from './componentes/Simples'
+import ParImpar from './componentes/ParImpar'
+import Inverter, {MegaSena} from './componentes/Multi'
+
+export default createDrawerNavigator({
+    MegaSena: {
+        screen: () => <MegaSena />,
+        navigationOptions: {title: 'Mega Sena'}
+    },
+    Inverter: {
+        screen: () => <Inverter texto="React Nativo!"/>
+    },
+    ParImpar: {
+        screen: () => <ParImpar numero={30} />,
+        navigationOptions: {title: 'Par & Ímpar'}
+    },
+    Simples: {
+        screen: () => <Simples texto="Simples" nome="Gustavo" />
+    }
+}, {drawerWidth: 300}) // linha que esoecifica a largura do drawer

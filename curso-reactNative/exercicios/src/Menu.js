6dd50b151f19e0cd1bb17fae630fff58cc7f65d2
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-06-14 19:07:50 -0300

    Contador para explicar a manipulacao de componet

--- curso-reactNative/exercicios/src/Menu.js
+++ curso-reactNative/exercicios/src/Menu.js
@@ -4,15 +4,19 @@ import {createDrawerNavigator} from 'react-navigation'
 import Simples from './componentes/Simples'
 import ParImpar from './componentes/ParImpar'
 import {Inverter, MegaSena} from './componentes/Multi'
+import Contador from './componentes/Contador'
 
 
 export default createDrawerNavigator({
+    Contador: {
+        screen: () => <Contador numeroInicial={10} />
+    },
     MegaSena: {
         screen: () => <MegaSena />,
         navigationOptions: {title: 'Mega Sena'}
     },
     Inverter: {
-        screen: () => <Inverter texto="React Nativo!"/>
+        screen: () => <Inverter texto="React nativo!"/>
     },
     ParImpar: {
         screen: () => <ParImpar numero={30} />,

d21f224e6bfd6e3d7a1b2c3017fa4a6322e40849
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-06-21 13:07:55 -0300

    implementado todo capo minado(corrrigir bug won)

--- curso-reactNative/mines/src/components/Flag.js
+++ curso-reactNative/mines/src/components/Flag.js
@@ -4,10 +4,10 @@ import { View, StyleSheet } from 'react-native'
 export default props => {
     return (
         <View style={styles.container}>
-            <View style={[styles.flagpole, props.bigger ? flagpoleBigger : null ]} />
-            <View style={[styles.flag, props.bigger ? flagBigger : null ]} />
-            <View style={[styles.base1, props.bigger ? base1Bigger : null ]} />
-            <View style={[styles.base2, props.bigger ? base2Bigger : null ]} />
+            <View style={[styles.flagpole, props.bigger ? styles.flagpoleBigger : null ]} />
+            <View style={[styles.flag, props.bigger ? styles.flagBigger : null ]} />
+            <View style={[styles.base1, props.bigger ? styles.base1Bigger : null ]} />
+            <View style={[styles.base2, props.bigger ? styles.base2Bigger : null ]} />
         </View> 
     )
 }

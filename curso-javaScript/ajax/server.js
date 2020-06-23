6d8b3a0156fd69f900f5aee6e3b1f584320de761
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-06-23 16:58:31 -0300

    add alguns comentários

--- curso-javaScript/ajax/server.js
+++ curso-javaScript/ajax/server.js
@@ -2,9 +2,9 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 
-app.use(express.static('.'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(express.static('.')) // provê todos os arquivos staticos da pasta
+app.use(bodyParser.urlencoded({ extended: true })) //lê os dados de um formulario e transforma em obj
+app.use(bodyParser.json()) //le a requisição e traduz para JSON
 
 app.get('/teste', (req, res) => res.send(new Date))
 app.listen(8080, () => console.log('Executando...'))

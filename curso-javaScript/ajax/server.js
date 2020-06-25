0ae7fe3e698b28d36f127661618195eca4143d6b
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-06-25 15:48:35 -0300

    ajax up arquivo não funciona

--- curso-javaScript/ajax/server.js
+++ curso-javaScript/ajax/server.js
@@ -1,10 +1,32 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const multer = require('multer')
+
 
 app.use(express.static('.')) // provê todos os arquivos staticos da pasta
 app.use(bodyParser.urlencoded({ extended: true })) //lê os dados de um formulario e transforma em obj
 app.use(bodyParser.json()) //le a requisição e traduz para JSON
+
+const storage = multer.diskStorage({
+    destinatio: function(req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function(req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.send('Ocorreu um erro')
+        }
+        res.send('Conlcuído com sucesso.')
+    })
+})
 
 app.get('/teste', (req, res) => res.send(new Date))
 app.listen(8080, () => console.log('Executando...'))

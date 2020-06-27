1d1cc4304d75bfcb6d7680cffb25e21cc5a5c265
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-06-27 15:30:29 -0300

    botão de marcar e desmarcar

--- curso-reactNative/tasks/src/screens/TaskList.js
+++ curso-reactNative/tasks/src/screens/TaskList.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { View, Text, ImageBackground, StyleSheet } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet, FlatList } from 'react-native'
 import todayImage from '../../assets/imgs/today.jpg'
 import moment from 'moment'
 import 'moment/locale/pt-br'
@@ -8,6 +8,31 @@ import Task from '../components/Task'
 
 
 export default class TaskList extends Component {
+
+    state = {
+        tasks: [{
+            id: Math.random(),
+            desc: "Comprar livro",
+            estimateAt: new Date(),
+            doneAt: new Date(),
+        },{
+            id: Math.random(),
+            desc: "Comprar livro",
+            estimateAt: new Date(),
+            doneAt: null,
+        }],
+    }
+
+    toggleTask = taskId => {
+        const tasks = [...this.state.tasks]
+        tasks.forEach(task => {
+            if(task.id === taskId) {
+                task.doneAt = task.doneAt ? null : new Date()
+            }
+        })
+        this.setState({tasks})
+    }
+
     render() {
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
 
@@ -20,8 +45,10 @@ export default class TaskList extends Component {
                     </View>
                 </ImageBackground>
                 <View style={styles.taskList}>
-                    <Task desc="tarefa 01" estimateAt={new Date()} doneAt={new Date()} />
-                    <Task desc="tarefa 02" estimateAt={new Date()} doneAt={null}/>
+                    <FlatList data={this.state.tasks}
+                        keyExtractor={item => `${item.id}`}
+                        renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask}/>}
+                    />
                 </View>
             </View>
         )
